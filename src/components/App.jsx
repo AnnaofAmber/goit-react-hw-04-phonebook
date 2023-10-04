ff948708729b0ba6fe4ec4d5caad09a43dab0ca3
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-04 21:11:06 +0300

    refactoring of App for hooks
    changed state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+
 import { nanoid } from 'nanoid';
 import css from './App.module.css';
 
@@ -7,44 +8,49 @@ import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  onSubmit = data => {
-    this.setState(prevState => {
+export const App = () => {
+  // state = {
+  //   contacts: [
+  // { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+  // { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+  // { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+  // { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   filter: '',
+  // };
+  const [contacts, setContacts] = useState([
+    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+  const onSubmit = data => {
+    setContacts(prevState => {
       data.id = nanoid();
       return { contacts: [...prevState.contacts, data] };
     });
   };
 
-  nameAlreadyExists = contact => {
-    return this.state.contacts.some(({ name }) => name === contact);
+  const nameAlreadyExists = contact => {
+    return contacts.some(({ name }) => name === contact);
   };
-  numberAlreadyExists = contact => {
-    return this.state.contacts.some(({ number }) => number === contact);
-  };
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const numberAlreadyExists = contact => {
+    return contacts.some(({ number }) => number === contact);
   };
 
-  onFilter = () => {
-    return this.state.contacts.filter(
+  // const onChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+
+  const onFilter = () => {
+    return contacts.filter(
       contact =>
-        contact.name.toLowerCase().match(this.state.filter.toLowerCase()) ||
-        contact.number.match(this.state.filter)
+        contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  onDelete = contactId => {
+  const onDelete = contactId => {
     this.setState(prevState => {
       return {
         contacts: prevState.contacts.filter(
@@ -54,34 +60,32 @@ export class App extends Component {
     });
   };
 
-  componentDidMount(){
-    const parsedContacts = JSON.parse(localStorage.getItem('contacts'))
-    if(parsedContacts){
-      this.setState({contacts:parsedContacts})
-    }
-  }
+  // componentDidMount(){
+  //   const parsedContacts = JSON.parse(localStorage.getItem('contacts'))
+  //   if(parsedContacts){
+  //     this.setState({contacts:parsedContacts})
+  //   }
+  // }
 
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.contacts !== prevState.contacts){
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
+  // componentDidUpdate(prevProps, prevState){
+  //   if(this.state.contacts !== prevState.contacts){
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  //   }
+  // }
 
-  render() {
-    return (
-      <div className={css.main}>
-        <h1 className={css.title}>Phonebook</h1>
-        <ContactForm
-          onSubmit={this.onSubmit}
-          nameAlreadyExists={this.nameAlreadyExists}
-          numberAlreadyExists={this.numberAlreadyExists}
-        />
+  return (
+    <div className={css.main}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm
+        onSubmit={onSubmit}
+        nameAlreadyExists={nameAlreadyExists}
+        numberAlreadyExists={numberAlreadyExists}
+      />
 
-        <Contacts title="Contacts">
-          <Filter filter={this.state.filter} onChange={this.onChange} />
-          <ContactList contacts={this.onFilter()} onDelete={this.onDelete} />
-        </Contacts>
-      </div>
-    );
-  }
-}
+      <Contacts title="Contacts">
+        <Filter filter={filter} onChange={setFilter} />
+        <ContactList contacts={onFilter()} onDelete={onDelete} />
+      </Contacts>
+    </div>
+  );
+};
